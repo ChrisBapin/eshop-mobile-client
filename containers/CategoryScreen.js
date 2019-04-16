82098542c009ea859bd91b659060ffa358e7d91d
Chris <cbapin@partenaire-intersport.fr>
2019-04-16 23:04:00 +0200

    add CategoryScreen

--- containers/CategoryScreen.js
+++ containers/CategoryScreen.js
@@ -1,84 +1,92 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow
-//  */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-// import React, { Component } from "react";
-// import {
-//   Platform,
-//   StyleSheet,
-//   Text,
-//   View,
-//   TextInput,
-//   TouchableOpacity,
-// } from "react-native";
-// import axios from "axios";
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  AsyncStorage,
+} from "react-native";
+import axios from "axios";
 
-// // const instructions = Platform.select({
-// //   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-// //   android:
-// //     "Double tap R on your keyboard to reload,\n" +
-// //     "Shake or press menu button for dev menu",
-// // });
-
-// type Props = {};
-// export default class App extends Component<Props> {
-//   state = {
-//     category: "",
-//   };
-
-//   handleSubmit = async () => {
-//     try {
-//       await axios.post("http://192.168.1.39:8080/create-category", {
-//         title: this.state.category,
-//       });
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-//   render() {
-//     console.log(this.state.category);
-//     return (
-//       <View style={styles.container}>
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Rentrez le nom d'une collection"
-//           onChangeText={category => this.setState({ category })}
-//         />
-//         <TouchableOpacity onPress={this.handleSubmit}>
-//           <Text>valider</Text>
-//         </TouchableOpacity>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: "#F5FCFF",
-//   },
-//   input: {
-//     borderWidth: 1,
-//     borderColor: "black",
-//     borderRadius: 5,
-//     padding: 10,
-//   },
+// const instructions = Platform.select({
+//   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+//   android:
+//     "Double tap R on your keyboard to reload,\n" +
+//     "Shake or press menu button for dev menu",
 // });
 
-import React from "react";
-import { Text } from "react-native";
+type Props = {};
+export default class App extends Component<Props> {
+  state = {
+    category: "",
+  };
 
-class CategoryScreen extends React.Component {
+  handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        "http://192.168.1.39:8080/create-category",
+        {
+          title: this.state.category,
+        }
+      );
+      // if(response.data._id)
+      console.log("cat", response.data);
+      if (response.data._id) {
+        const value = JSON.stringify(response.data);
+        await AsyncStorage.setItem("categoryInfo", value);
+        console.log("value", value);
+        //redirection vers page Product
+        this.props.navigation.navigate("Product", {
+          title: response.data.title,
+        });
+      } else {
+        await AsyncStorage.setItem("categoryInfo", "");
+        alert(response.data);
+        this.setState({ category: "" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
-    return <Text>CategoryScreen</Text>;
+    console.log(this.state.category);
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Rentrez le nom d'une collection"
+          onChangeText={category => this.setState({ category })}
+        />
+        <TouchableOpacity onPress={this.handleSubmit}>
+          <Text>valider</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
 }
 
-export default CategoryScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 5,
+    padding: 10,
+  },
+});
