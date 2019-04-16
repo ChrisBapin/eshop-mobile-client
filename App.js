73f8461086986b7d0ea81fe97c8a9895c0b06085
Chris <cbapin@partenaire-intersport.fr>
2019-04-16 10:45:07 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import {
+  createSwitchNavigator,
+  createStackNavigator,
+  createAppContainer,
+} from "react-navigation";
+
+// import AuthLoadingScreen from "./containers/AuthLoadingScreen";
+// import SignInScreen from "./containers/SignInScreen";
+// import SignUpScreen from "./containers/SignUpScreen";
+import CategoryScreen from "./containers/CategoryScreen";
+import ProductScreen from "./containers/ProductScreen";
+
+const AppStack = createStackNavigator({
+  // Tab: TabNavigator,
+  // SignIn: SignInScreen,
+  // SignUp: SignUpScreen,
+  Product: ProductScreen,
+  Category: CategoryScreen,
+});
+// const AuthStack = createStackNavigator({
+//   // SignIn: SignInScreen,
+//   // SignUp: SignUpScreen,
+// });
+
+// export default createAppContainer(
+//   createSwitchNavigator(
+//     {
+//       // AuthLoading: AuthLoadingScreen,
+//       // Category: CategoryScreen,
+//       App: AppStack,
+//       Auth: AuthStack,
+//     },
+//     {
+//       initialRouteName: "App",
+//     }
+//   )
+// );
+
+const App = createAppContainer(AppStack);
+
+export default App;
